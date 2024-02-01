fb3957ea64ba036d464cb3ed08c04527d3051ecc
Amkrlv <amkrlv@icloud.com>
2024-02-01 09:58:54 +0300

    2

--- main.py
+++ main.py
@@ -1,12 +1,21 @@
 import pandas as pd
+import streamlit as st
 
 
-df=pd.read_excel(
-    io="ТАКСИ ЗАДАНИЕ 1.xlsx",
-    engine="openpyxl",
-    sheet_name="LOL",
-    skiprows=0,
-    usecols="A:D",
-    nrows=16,
+
+st.set_page_config(page_title="Supermarket",
+                              page_icon=":rainbow:",
+                              layout="wide"
 )
-print(df)
+
+
+
+df=pd.read_excel(io="supermarkt_sales.xlsx",
+                 engine="openpyxl",
+                 sheet_name="Sales",
+                 skiprows=3,
+                 usecols="B:R",
+                 nrows=1000,
+)
+df_selection=(df.iloc[350:550])
+st.dataframe(df_selection)
